e5e180148ea00b9cda9d4538dafcb8b431a86f2f
vminanda <vminanda@gmail.com>
2025-04-21 22:29:32 +0800

    adding loop for libs installation

--- classifier.py
+++ classifier.py
@@ -1,3 +1,16 @@
+#installing some libs
+import subprocess
+import sys
+
+# List your required packages
+required_packages = ['pandas', 'numpy', 'scikit-learn']
+
+for package in required_packages:
+    try:
+        __import__(package)
+    except ImportError:
+        subprocess.check_call([sys.executable, '-m', 'pip', 'install', package])
+
 #importing some libs
 import pandas as pd
 import numpy as np
@@ -6,3 +19,6 @@ import matplotlib.pyplot as plt
 #read dataset:
 data = pd.read_csv("Credit Score Classification Dataset.csv")
 data.head()
+
+#Dana Analysis
+data.shape()
