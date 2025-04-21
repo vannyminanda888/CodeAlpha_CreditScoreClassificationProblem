b210ca643cf77ef6a4499b8f96f5265b25142bd2
vminanda <vminanda@gmail.com>
2025-04-21 15:27:15 +0800

    adding dataset path

---
+++ classifier.py
@@ -0,0 +1,8 @@
+#importing some libs
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+#read dataset:
+data = pd.read_csv("Credit Score Classification Dataset.csv")
+data.head()
