a867b64c3bc813c02a6040c2af7cf64bb92745bb
vminanda <vminanda@gmail.com>
2025-04-21 22:35:00 +0800

    removing a module

--- classifier.py
+++ classifier.py
@@ -14,7 +14,6 @@ for package in required_packages:
 #importing some libs
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 
 #read dataset:
 data = pd.read_csv("Credit Score Classification Dataset.csv")
